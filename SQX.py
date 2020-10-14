8e689d067aacae4fd87f93305eb12a999354d342
nnnnnzo <44113165+nnnnnzo@users.noreply.github.com>
2020-10-14 09:25:50 +0200

    Update and rename fibopls.py to SQX.py

---
+++ SQX.py
@@ -0,0 +1,21 @@
+import turtle
+def squarx(t, h, p):
+    if p!=0:
+        t.forward(h)
+        t.right(90)
+        t.forward(h)
+        t.right(90)
+        t.forward(h)
+        t.right(90)
+        t.forward(h)
+        t.right(90)
+        t.right(+10)
+        squarx(t, h-3, p-1)
+
+t = turtle.Turtle()
+wn=turtle.Screen()
+wn.bgcolor("black")
+wn.title("SquareX: Fibonacci en pls")
+t.color("#d79a10", "black")
+t.speed(0)
+squarx(t, 200, 70)
